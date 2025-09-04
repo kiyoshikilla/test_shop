07389c99cc2c554fc785e21dbafe2cc7a7d92cca
Vitaliy Lehkii <ondihrmang@gmail.com>
2025-09-04 17:13:04 +0300

    fix migrations bug

--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from users.models import AbstractUser
+from users.forms import UserLoginForm, UserCreationForm 
 
 # Create your views here.
+
+def login(request):
+    context = {'form' : UserLoginForm}
+    return render(request, 'users/login.html', context)
+
+def register(request):
+    context = {'form' : UserCreationForm}
+    return render(request, 'users/register.html', context)
