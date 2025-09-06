ed90c1ee79a282c983df1d7c9dafafec1c9f1d34
Vitaliy Lehkii <ondihrmang@gmail.com>
2025-09-06 17:52:51 +0300

    linked login controller

--- users/views.py
+++ users/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
 from users.models import AbstractUser
 from users.forms import UserLoginForm, UserRegisterForm 
+from django.contrib import auth
 
 # Create your views here.
 
 def login(request):
-    context = {'form' : UserLoginForm}
+    if request.method == "POST":
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+    else:
+        form = UserLoginForm()
+        
+    context = {'form' : form}
     return render(request, 'users/login.html', context)
 
 def register(request):
