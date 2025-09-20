93acb826133ea18c543beb43db0ae2a5dea029d7
Vitaliy Lehkii <ondihrmang@gmail.com>
2025-09-21 01:51:11 +0300

    cart model + html integration

--- products/urls.py
+++ products/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from products.views import shop, detail
+from products.views import shop, detail, cart
 
 app_name= 'products'
 
 urlpatterns = [
     path('', shop, name='shop'),
     path('details/<int:pk>/', detail, name='detail'),
-    
+    path('cart/', cart, name="cart")
 ]
 
--- products/views.py
+++ products/views.py
@@ -32,3 +32,6 @@ def shop(request):
 def detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     return render(request, 'products/detail.html', {'product' : product})
+
+def cart(request):
+    return render(request, "products/basket.html", {"cart" : cart})
